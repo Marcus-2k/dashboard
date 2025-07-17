5aae8c0383ed4208c36837611a8d097e7c3818fd
Marcus <marcus.petrihen@gmail.com>
2025-07-17 20:07:31 +0300

    init nest server

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import MinioController from "./controllers/minio.controller";
+import ProductController from "./controllers/product.controller";
+import { DatabaseModule } from "./modules/database/database.module";
+
+const controllers = [ProductController, MinioController];
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
+  controllers: controllers,
   providers: [],
   exports: [],
 })
